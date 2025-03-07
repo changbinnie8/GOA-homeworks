e71ac349a19e4fe744a206507d104c5cf21fe113
changbinnie8 <kelbakiani.mariami2011@gmail.com>
2025-03-07 23:39:39 +0400

    commit

---
+++ day06/classwork/classwork.py
@@ -0,0 +1,13 @@
+Name = 'mari'
+print(Name)
+goa_best = 100
+print(goa_best)
+name = 'Mari'
+print(name)
+number = 10
+print(number)
+goaBest = 100
+print(goaBest)
+
+#case-sensetive ნიშნავს რომ დიდი და პატარა ასოები განსხვავებულად განიხილება
+#snake case არის წერის სტილი სადაც სიტყვები გამოყოფილია ქვედა ტირეთი 
---
+++ day06/homework/homework.py
@@ -0,0 +1,24 @@
+age = input("enter your age: ")
+name = input("enter your first and last name: ")
+birthDay = input("enter your birth day: ")
+
+#camel-case გამოიყენება ორი ან ორზე მეტი სიტყვის დასაწერად. ყოველი ახალი სიტყვის დაწყებისას ვიწყებთ დიდი შრიფტით.
+#snake-case იგივე მიზნით გამოიყენება. განსხვავება არის ის რომ სიტყვების დასაშრებლად ვიყენებთ ქვედა ტირეს.
+#case-sensitive არის როდესაც ყურადღება ექცევა სიტყვაში გამოყენებული ასოების შრიფტს.
+
+# რა არის debuging? ასევე გაასწორე ეს Eror ები:
+# 1)
+# name = 'zura'
+# print(name)
+# 2)
+# age = 13
+# print(age)
+# 3)
+# Hello_world = "Hello"
+# print(Hello_world)
+# 4)
+# name = 'nika'
+# print(name)
+# 5)
+# year = 2025
+# print(year)
