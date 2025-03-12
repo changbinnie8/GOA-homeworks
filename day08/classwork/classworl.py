d512098529ae901673a58766d43402f930b27949
changbinnie8 <kelbakiani.mariami2011@gmail.com>
2025-03-12 18:46:29 +0400

    commit

---
+++ day08/classwork/classworl.py
@@ -0,0 +1,25 @@
+#type() ფუნქცია გვეუბნება, თუ რა ტიპის არის მონაცემი
+name = "mari"
+age = 13
+height = 161.5
+bool1 = True
+bool2 = False
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(bool1))
+print(type(bool2))
+
+num1 = 100
+num2 = 27
+
+add = num1 + num2
+substraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(add)
+print(substraction)
+print(multiplication)
+print(division)
---
+++ day08/homework/homework.py
@@ -0,0 +1,16 @@
+#string "mariami"
+#integer 13
+#float 12.7
+#boolean True and False
+
+num1 = 19
+num2 = 59
+print(num1 + num2)
+
+#არასწორი იმიტომ იყო რომ პირველი ცვლადის მნიშვნელობობა იყო სტრინგი. სტრინგებსა და ინტეჯერებს შორის, ვერ შესრულდება მათემატიკური ოპერაცია.
+
+str1 = "hello"
+str2 = "my name"
+str3 = "is"
+str4 = "mariam"
+print(str1 + " " + str2 + " " + str3 + " " + str4)
