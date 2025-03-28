576111d356ec466ba90ef7fe9e5818542bb6fa1c
changbinnie8 <kelbakiani.mariami2011@gmail.com>
2025-03-28 17:15:41 +0400

    commit

--- day10/homework/homework.py
+++ day10/homework/homework.py
@@ -5,3 +5,12 @@ print(74 >= 98)
 print(23 != 65)
 
 #2
+num1 = input("enter a number: ")
+num2 = input("enter a number: ")
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
+
+#3
+num3 = input("enter a number: ")
+print(num3 == 10)
