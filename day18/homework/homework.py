7bf9243c103b0a0bae32800bb15bc08336facca3
changbinnie8 <kelbakiani.mariami2011@gmail.com>
2025-04-13 19:11:11 +0400

    commit

---
+++ day18/homework/homework.py
@@ -0,0 +1,8 @@
+for i in range(1000):
+    print("GOA BEST")
+
+#for loop გამოიყენება კოდის გასმეორებლად და როცა ვიცით გამეორება რამდენჯერ უნდა მოხდეს.
+
+num = int(input("enter any number"))
+for i in range(num):
+    print("GOA BEST")
